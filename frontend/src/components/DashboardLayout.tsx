53979fc71f5fe35bc077ddf633d427afc187f9f6
gpi <gpimpale29@gmail.com>
2021-02-27 09:33:41 -0800

    nice

--- frontend/src/components/DashboardLayout.tsx
+++ frontend/src/components/DashboardLayout.tsx
@@ -5,7 +5,7 @@ import InnerLayout from '../components/InnerLayout';
 
 export default function DashboardLayout(props: React.PropsWithChildren<AuthenticatedComponentProps>) {
   return <InnerLayout name={props.apiKey.creator.name} logoutCallback={() => props.setApiKey(null)} >
-    <InnerLayout.SidebarEntry label="Dashboard" icon={Dashboard} href="/" />
+    <InnerLayout.SidebarEntry label="Dashboard" icon={Dashboard} href="/dashboard" />
     <InnerLayout.SidebarEntry label="Calendar" icon={Event} href="/calendar" />
     <InnerLayout.SidebarEntry label="Settings" icon={Settings} href="/settings" />
     <InnerLayout.Body>
