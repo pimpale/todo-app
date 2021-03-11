1e34623bdba4d169399bbb3e72c7c59756a98010
warrenwallis <66290894+warrenwallis@users.noreply.github.com>
2021-03-11 15:40:32 -0800

    Update Instructions.tsx
    Begin instructions pages

--- frontend/src/pages/Instructions.tsx
+++ frontend/src/pages/Instructions.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import SimpleLayout from '../components/SimpleLayout';
+import Section from '../components/Section';
 
-// Bootstrap CSS & JS
-import '../style/dashboard.scss';
-import 'bootstrap/dist/js/bootstrap';
-import 'popper.js/dist/popper';
-
-function Error404() {
-  return (
-    <div style={{
-      display: "flex",
-      height: "100vh",
-      width: "100vw"
-    }}>
-      <div className="my-auto mx-auto text-center">
-        <h1>Instructions.</h1>
-        <h5>Page under construction</h5>
-        <Link to="/">Return Home</Link>
-      </div>
-    </div>
-  )
+function Instructions() {
+  return <SimpleLayout>
+    <section>
+      
+    </section>
+  </SimpleLayout>
 }
 
-export default Error404
+export default Instructions;
