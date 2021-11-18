ddb7a9b4ef99210209bfc1e9c21598f9e23fa3cf
Nathan Wei <njwei@g.ucla.edu>
2021-11-17 16:09:30 -0800

    fixed the shape in homescreen

--- web-frontend/src/pages/Home.tsx
+++ web-frontend/src/pages/Home.tsx
@@ -58,6 +58,8 @@ function Home(props: BrandedComponentProps) {
     backgroundColor: "#F6F0E5"
   };
 
+  
+
   const homeBody = {
     height: "50vh",
     marginTop: "110px"
@@ -80,10 +82,10 @@ function Home(props: BrandedComponentProps) {
       </Container>
 
       
-      <div className="custom-shape-divider-bottom-1624938662">
+      <div className="w-100">
         <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
             <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,
-            11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
+            11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill" fill="#F6F0E5"></path>
         </svg>
       </div>
 
