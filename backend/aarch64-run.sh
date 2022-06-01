27b0fb9ee4a9133ae357bb88243ede7c246896c5
gpi <gpimpale29@gmail.com>
2022-05-31 23:33:10 -0700

    update settings

--- backend/aarch64-run.sh
+++ backend/aarch64-run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 exec ./aarch64/todo-app-service \
-  --port=18080 \
+  --port=10080 \
   --database-url=postgres://postgres:toor@localhost/todo_app \
-  --site-external-url=http://todoapp.innexgo.com \
-  --auth-service-url=http://localhost:8079
+  --site-external-url=http://todoapp.eaucla.org \
+  --auth-service-url=http://localhost:10079
