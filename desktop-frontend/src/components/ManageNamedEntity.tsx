1ffd493bb4802d7f0580afa0be4feee5f37bcbaa
gpi <gpimpale29@gmail.com>
2022-12-26 12:45:46 -0800

    update

--- desktop-frontend/src/components/ManageNamedEntity.tsx
+++
@@ -1,7 +0,0 @@
-import { NamedEntityData, NamedEntityPattern } from "@innexgo/frontend-todo-app-api";
-
-export type TagData = {
-  ned: NamedEntityData,
-  nep: NamedEntityPattern[],
-}
-
--- desktop-frontend/src/react-app-env.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '*.png'
-declare module '*.wav'
-declare module '*.mov'
-declare module '*.webm'
